e68b7117ce2fce6840311a6b5ada792125c54519
Daniel Oliveira <gbdaniel_rj@hotmail.com>
2018-04-11 00:30:57 -0300

    não permitir mensagens com o objeto JSON inválido

--- src/main/java/br/com/backend/exceptionHandler/ProjectExceptionHandler.java
+++ src/main/java/br/com/backend/exceptionHandler/ProjectExceptionHandler.java
@@ -1,8 +1,12 @@
 package br.com.backend.exceptionHandler;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -13,6 +17,13 @@ public class ProjectExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handlePessoaInexistenteOuInativaException( EntidadeNaoEncontradaException ex ) {
 
         return ResponseEntity.badRequest().body( ex.getMessage() );
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable( HttpMessageNotReadableException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request ) {
+
+        return handleExceptionInternal( ex, ex.getMessage(), headers, HttpStatus.BAD_REQUEST, request );
     }
 
 }
